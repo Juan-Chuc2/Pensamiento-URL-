7f876f3b3bd355dc273818abe898d336d04d4fa0
Juan-Chuc2 <chucjuan508@gmail.com>
2025-03-26 11:49:17 -0600

    Add files via upload

---
+++ Ejercicios y Escenarios .py
@@ -0,0 +1,27 @@
+# Ejercicio 1
+#for i in range(1,11):
+ #  if i%2:
+#
+#    print(i)
+
+# ejercicio 2
+#x=1
+#while x<11:
+  #  if x %2 ==1:
+ #       print(x)
+#    x +=1
+
+# Escenario 1
+#t=input("ingrese un texto ")
+#while t!="chupacabra":
+    #print("continuemos")
+   # t=input("ingrese un texto ")
+  #  if t== "chupacabra":
+ #       print("Has dejado el bucle con exito")
+#        break
+
+#escenario 2
+user_word= input("ingrese una palabra : ")
+user_word= user_word.upper()
+for vocal in user_word:
+    if vocal in ['A', 'E','I',]
